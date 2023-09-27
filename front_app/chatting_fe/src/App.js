46f58b46f205404d44d603c85066ab113c900fdb
hjuuK <khj020508@naver.com>
2023-09-27 20:20:27 +0900

    리액트 기초

--- front_app/chatting_fe/src/App.js
+++ front_app/chatting_fe/src/App.js
@@ -1,15 +1,31 @@
-import styled from './common.module.scss';
+import styled, {css} from 'styled-components';
+
+const StyleButton = styled.button`
+    width: 300px;
+    height: 50px;
+    background: ${props => props.bg || 'black'};
+    border: 0;
+    color: #fff;
+
+    ${props => props.border && css`
+      border: 10px solid blue;
+      border-radius: 5px;
+    `}
+
+    &:hover {
+      background: gray;
+    }
+
+    i {
+      font-size: 2rem;
+      color: orange;
+    }
+`;
 
 const App = () => {
   
-
   return (
-    <div className={styled.container}>
-      <button className={styled.red}>클릭</button>
-      <button className='orange'>클릭</button>
-      <button className='green'>클릭</button>
-      <button className={styled.blue}>클릭</button>
-    </div>
+    <StyleButton bg="red" border={true}><i>1</i>클릭!</StyleButton>
   );
 };
 
