941ab35ae15ffc6792afde17cc744ab6c480cd38
hjuuK <khj020508@naver.com>
2023-10-04 19:34:36 +0900

    프론트 앱 구성

--- front_app/chatting_fe/src/App.js
+++ front_app/chatting_fe/src/App.js
@@ -3,6 +3,8 @@ import CommonLayout from './layouts/CommonLayout';
 import MainPage from './pages/MainPage';
 import Rooms from './pages/Rooms';
 import Room from './pages/Room';
+import Join from './pages/Join';
+import Login from './pages/Login';
 import NotFound from './pages/NotFound';
 
 const App = () => {
@@ -13,6 +15,8 @@ const App = () => {
         <Route path="/room" element={<Rooms />}>
           <Route path=":rommNo" element={<Room />} />
         </Route>
+        <Route path="/join" element={<Join/>} />
+        <Route path="/login" element={<Login/>} />
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
