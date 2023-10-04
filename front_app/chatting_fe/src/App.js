c249e941defa6ba399e112c1f8550f75c888bca9
hjuuK <khj020508@naver.com>
2023-10-04 19:14:59 +0900

    1004 리액트 강의

--- front_app/chatting_fe/src/App.js
+++ front_app/chatting_fe/src/App.js
@@ -1,10 +1,23 @@
-import { StyleButton } from "./components/commons/Buttons";
+import { Routes, Route } from 'react-router-dom';
+import CommonLayout from './layouts/CommonLayout';
+import MainPage from './pages/MainPage';
+import Rooms from './pages/Rooms';
+import Room from './pages/Room';
+import NotFound from './pages/NotFound';
 
 const App = () => {
-  
   return (
-    <StyleButton bg="red" border={true}><i>1</i>클릭!</StyleButton>
+    <Routes>
+      <Route element={<CommonLayout />}>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/room" element={<Rooms />}>
+          <Route path=":rommNo" element={<Room />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   );
+  
 };
 
 export default App;
--- front_app/chatting_fe/src/components/commons/Buttons.js
+++ front_app/chatting_fe/src/components/commons/Buttons.js
@@ -1,4 +1,4 @@
-import styled, {css} from "styled-components";
+import styled, {css} from 'styled-components';
 
 export const StyleButton = styled.button`
     width: 300px;
