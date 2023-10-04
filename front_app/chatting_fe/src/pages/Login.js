26997bbab4fc6bdf472bc5a2533e0ae048d86b79
hjuuK <khj020508@naver.com>
2023-10-04 20:51:55 +0900

    프론트 앱 구성

--- front_app/chatting_fe/src/pages/Login.js
+++ front_app/chatting_fe/src/pages/Login.js
@@ -2,6 +2,7 @@
 //import { useLocation } from "react-router-dom";
 //import { useSearchParams } from "react-router-dom";
 import { useState } from "react";
+import ErrorMessage from "../components/commons/ErrorMessage";
 
 const Login = () => {
     //const [count, setCount] = useState(0);
@@ -28,6 +29,8 @@ const Login = () => {
         userPw : '',
     });
 
+    const [message, setMessage] = useState('');
+
     const handleChange = (e) => {
         form[e.target.name] = e.target.value;
         setForm({...form});
@@ -35,15 +38,23 @@ const Login = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        if (!form.userId.trim()) {
+            setMessage("아이디를 입력하세요.");
+            return;
+        }
 
+        if (!form.userPw.trim()) {
+            setMessage("비밀번호를 입력하세요.");
+            return;
+        }
     };
 
     return (
         <form onSubmit={handleSubmit}>
             <input type="text" name="userId" placeholder="아이디" onChange={handleChange} /><br />
             <input type="password" name="userPw" placeholder="비밀번호" onChange={handleChange} /><br />
+            { message && <ErrorMessage>{message}</ErrorMessage>}
             <button type="submit">로그인</button>
-            {form.userId}
         </form>
     )
     
