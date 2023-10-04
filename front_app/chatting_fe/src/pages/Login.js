02ec9510ab1cb00b2a9479b2f0b440a6e6b06a48
hjuuK <khj020508@naver.com>
2023-10-04 20:37:54 +0900

    프론트 앱 구성

--- front_app/chatting_fe/src/pages/Login.js
+++ front_app/chatting_fe/src/pages/Login.js
@@ -1,12 +1,52 @@
-import { useNavigate } from "react-router-dom";
+//import { useNavigate } from "react-router-dom";
+//import { useLocation } from "react-router-dom";
+//import { useSearchParams } from "react-router-dom";
+import { useState } from "react";
 
 const Login = () => {
-    const isLogin = true;
+    //const [count, setCount] = useState(0);
+    
+    /*
+    const [searchParams, setSearchParams ] = useSearchParams();
+    const test = searchParams.get('test');
+    setSearchParams({t1: 1, t2: 2, t3: 3});
+    
+    //const isLogin = true;
+    //const location = useLocation();
+    //console.log(location);
+    //location.href="/room";
+    
     const navigate = useNavigate();
     if (isLogin) {
-        navigate("/");
+        //navigate("/", { replace: true });
+        navigate(-1); // history.go(-1)
     }
-    return <h1>로그인</h1>;
+    */
+
+    const [form, setForm] = useState({
+        userId : '',
+        userPw : '',
+    });
+
+    const handleChange = (e) => {
+        form[e.target.name] = e.target.value;
+        setForm({...form});
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="text" name="userId" placeholder="아이디" onChange={handleChange} /><br />
+            <input type="password" name="userPw" placeholder="비밀번호" onChange={handleChange} /><br />
+            <button type="submit">로그인</button>
+            {form.userId}
+        </form>
+    )
+    
 };
 
 export default Login;
