358cc1d82aeaa37a707ab083004d4f52ef4bd1d8
hjuuK <khj020508@naver.com>
2023-10-05 20:33:52 +0900

    프론트 앱 구성

--- front_app/chatting_fe/src/pages/Rooms.js
+++ front_app/chatting_fe/src/pages/Rooms.js
@@ -1,26 +1,11 @@
-import { useMemo, useState } from 'react';
 
-function factorial(num) {
-    let total = 1;
-    for (let i = 1; i <= num; i++) {
-        total *= i;
-    }
-    
-    return total;
-}
 
 const Rooms = () => {
-    const [num, setNum] = useState(0);
-
-    const handleChange = (e) => {
-        setNum(e.target.value);
-    };
-
-    const result = useMemo(() => factorial(num), [num]);
+    
 
     return (
         <>
-            <input type="number" onChange={handleChange} /> = {result}            
+           
         </>
     );
 };
