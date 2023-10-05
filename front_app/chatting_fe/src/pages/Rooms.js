7756f4216d1a53f143199c55eb29d3e54cdca984
hjuuK <khj020508@naver.com>
2023-10-05 20:54:21 +0900

    프론트 앱 구성

--- front_app/chatting_fe/src/pages/Rooms.js
+++ front_app/chatting_fe/src/pages/Rooms.js
@@ -5,23 +5,28 @@ import ErrorMessage from '../components/commons/ErrorMessage';
 const Rooms = () => {
     const [rooms, setRooms] = useState([]);
     const [message, setMessage] = useState('');
-
+    const [loading, setLoading[ = useState(false);
+    ]]
     useEffect(() => {
         getRooms()
             .then((data) => {
+                console.log(ㅂ기뱅)
                 setRooms(data)
             })
             .catch((err) => {
                 console.error(err);
-                setMessage("방목록 조회 실패...");
+                setMessage("방 목록 조회 실패...");
             })
     }, []);
 
+    if (rooms.setLoading > 0)}
+
     return (
         <>
-           {message && <ErrorMessage>{message}</ErrorMessage>}
-           
-        </>
+            {message && <ErrorMessage>{message}</ErrorMessage>}
+            
+
+        </> 
     );
 };
 
