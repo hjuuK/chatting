cbee41263eaddf4483a8f30db1c8e80cdbb82325
hjuuK <khj020508@naver.com>
2023-10-10 07:35:01 +0900

    프론트 방목록 구성

--- front_app/chatting_fe/src/pages/Rooms.js
+++ front_app/chatting_fe/src/pages/Rooms.js
@@ -1,10 +1,12 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState, useCallback } from "react";
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { getRooms } from "../api/chatting";
 import ErrorMessage from '../components/commons/ErrorMessage';
 import Title from '../components/commons/Title';
-import { StyleButton } from '../components/commons/Buttons';
+import { StyleButton } from "../components/commons/Buttons";
+import { registerRoom } from "../api/chatting";
 
 const RoomBox = styled.li`
     box-shadow: 2px 2px 5px #212121;
@@ -26,22 +28,25 @@ const FormBox = styled.form`
         width: 100%;
         text-align: center;
     }
-    button {
+    
+    input:focus {
+        border-color: #000; 
+    }
+
+    button { 
         margin-bottom: 20px;
     }
 `;
 
 const Rooms = () => {
-    const [form, setForm] = useState({roomNm: '', max : 0})
+    const [form, setForm] = useState({roomNm: '', max : ''})
     const [rooms, setRooms] = useState([]);
     const [message, setMessage] = useState('');
     const [loading, setLoading] = useState(true);
 
-
-    useEffect(() => {
+    const updateRooms = useCallback(() => {
         getRooms()
             .then((res) => {
-                console.log(data);
                 setRooms(res.data);
                 setLoading(false);
             })
@@ -50,44 +55,64 @@ const Rooms = () => {
                 setMessage("방목록 조회 실패...");
                 setLoading(false);
             });
-    }, []);
+    }, [])
+
+    useEffect(() => updateRooms(), []);
 
     const handleSubmit = useCallback((e) => {
         e.preventDefault();
-        console.log(form);
-    }, []);
+        try {
+            if (!form.roomNm) {
+                e.target.roomNm.focus();
+
+                throw new Error("방 이름을 입력하세요.");
+            }
+
+            registerRoom(form)
+                .then(() => {
+                    setForm({roomNm: '', max: ''});
+                    updateRooms();
+                })
+                .catch((err) => console.error(err));
+                
+        } catch(err) {
+            setMessage(err.message);
+        }
+
+    },[]);
 
     const handleChange = useCallback((e) => {
-        form[e.targer.name] = e.target.value;
-        setForm({ ...form });
+        form[e.target.name] = e.target.value.trim();
+        setForm({ ...form});
     }, []);
 
     let lis = null;
     if (rooms && rooms.length > 0) {
         lis = rooms.map(r => {
-        const link = `/room/${r.roomNo}`;
-        return (
-            <RoomBox key={r.roomNo}>
-                <Link to={link}>
-                    <div class='left'>{r.roomNm}</div>
-                    <div class='rigth'>최대 인원수 : {r.max}명</div>
-                </Link>
-            </RoomBox>);
-        });
+            const link = `/room/${r.roomNo}`;
+            return (
+                <RoomBox key={r.roomNo}>
+                    <Link to={link}>
+                        <div className='left'>{r.roomNm}</div>
+                        <div className='right'>최대 인원수 : {r.max}명</div>
+                    </Link>
+                </RoomBox>)
+            });
     }
     
     return (
         <>
             <Title>방 등록</Title>
-            <FormBox autoComplete="off" onSubmit={handleSubmit}>
-                <input type="text" name="roomNm" placeholder="방이름 입력" onChange={handleChange} />
-                <input type="number" name="max" placeholder="최대 인원수" onChange={handleChange} />
-                <StyleButton type='submit' width="100%">등록하기</StyleButton>
-            </FormBox>
+           <FormBox autoComplete="off" onSubmit={handleSubmit}>
+            <input type="text" name="roomNm" value={form.roomNm} placeholder="방이름 입력" onChange={handleChange} />
+            <input type="number" name="max" value={form.max} placeholder="최대 인원수" onChange={handleChange} />
+            <StyleButton type='submit' width="100%">등록하기</StyleButton>
+           </FormBox>
+
            {message && <ErrorMessage>{message}</ErrorMessage>}
            { loading && rooms.length === 0 && <div>로딩중....</div> }
             <Title>방 목록</Title>
-           <ul>
+            <ul>
                 {lis}
             </ul>
         </>
